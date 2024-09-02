6ba03ad66ecfa275c67f9c2589ae0866c0088803
Hagoeun <sera1193@naver.com>
2024-09-03 01:29:08 +0900

    feat: entity 생성

---
+++ member-service/src/main/java/com/songspasssta/memberservice/domain/Member.java
@@ -0,0 +1,39 @@
+package com.songspasssta.memberservice.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@DynamicInsert
+@NoArgsConstructor(access = PROTECTED)
+@SQLDelete(sql = "UPDATE member SET status = 'DELETED' where id = ?")
+@SQLRestriction("status = 'ACTIVE'")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String profileImageUrl;
+
+    @OneToMany(mappedBy = "member")
+    private List<Reward> rewards = new ArrayList<>();
+}
