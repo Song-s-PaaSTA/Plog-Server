8b95ec9895c0bbde483f168061cecd440a4abc60
Hagoeun <sera1193@naver.com>
2024-09-02 23:59:26 +0900

    feat: trash-service 생성

---
+++ trash-service/src/main/java/com/songspasssta/trashservice/TrashServiceApplication.java
@@ -0,0 +1,13 @@
+package com.songspasssta.trashservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrashServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrashServiceApplication.class, args);
+	}
+
+}
