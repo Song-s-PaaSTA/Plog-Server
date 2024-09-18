7bd72b82adab6161029659860bae14f64301c914
Hagoeun <sera1193@naver.com>
2024-09-18 23:38:26 +0900

    feat: 에러 코드 추가

--- common/src/main/java/com/songspasssta/common/exception/ExceptionCode.java
+++ common/src/main/java/com/songspasssta/common/exception/ExceptionCode.java
@@ -9,7 +9,10 @@ public enum ExceptionCode {
 
     INVALID_REQUEST(1000, "올바르지 않은 요청입니다."),
 
-    INTERNAL_SEVER_ERROR(9999, "서버에서 에러가 발생하였습니다.");
+    INTERNAL_SEVER_ERROR(9999, "서버에서 에러가 발생하였습니다."),
+
+    NOT_FOUND_MEMBER_ID(2000, "존재하지 않는 회원입니다."),
+    FAIL_TO_SOCIAL_LOGIN(2001, "소셜 로그인에 실패하였습니다.");
 
     private final int code;
     private final String message;
