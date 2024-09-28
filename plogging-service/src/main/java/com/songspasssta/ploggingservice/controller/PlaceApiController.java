3e017b0f302f30a07f7841211068ce04bc737ff3
khee2 <21keunghee@gmail.com>
2024-09-28 21:03:56 +0900

    comment: swagger 관련 주석 추가

--- plogging-service/src/main/java/com/songspasssta/ploggingservice/controller/PlaceApiController.java
+++ plogging-service/src/main/java/com/songspasssta/ploggingservice/controller/PlaceApiController.java
@@ -2,6 +2,11 @@ package com.songspasssta.ploggingservice.controller;
 
 import com.songspasssta.ploggingservice.dto.PlaceResponseDto;
 import com.songspasssta.ploggingservice.service.PlaceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,11 +18,18 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/place")
 @RequiredArgsConstructor
+@Tag(name = "Place API", description = "장소 관련 API를 제공하는 컨트롤러입니다.")
 public class PlaceApiController {
     private final PlaceService placeService;
 
     @GetMapping
-    public List<PlaceResponseDto> getLocationInfo(@RequestParam String query) {
+    @Operation(summary = "장소 정보 조회", description = "쿼리 문자열을 이용해 장소 정보를 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "장소 정보 조회 성공"),
+    })
+    public List<PlaceResponseDto> getLocationInfo(
+            @Parameter(description = "검색할 장소의 쿼리 문자열", example = "Seoul")
+            @RequestParam String query) {
         return placeService.getLocationInfo(query);
     }
 }
