16edf3907f56879d1d8aeec233d071737a4d5b16
khee2 <21keunghee@gmail.com>
2024-09-28 21:04:41 +0900

    feat: 장소 정보 조회 API에 대한 예외 처리 기능 추가

--- plogging-service/src/main/java/com/songspasssta/ploggingservice/service/PlaceService.java
+++ plogging-service/src/main/java/com/songspasssta/ploggingservice/service/PlaceService.java
@@ -1,8 +1,11 @@
 package com.songspasssta.ploggingservice.service;
 
+import com.songspasssta.common.exception.ExceptionCode;
+import com.songspasssta.common.exception.NaverApiException;
 import com.songspasssta.ploggingservice.client.NaverLocalSearchClient;
 import com.songspasssta.ploggingservice.dto.NaverSearchResponse;
 import com.songspasssta.ploggingservice.dto.PlaceResponseDto;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -27,17 +30,25 @@ public class PlaceService {
      */
 
     public List<PlaceResponseDto> getLocationInfo(String query) {
-        NaverSearchResponse response = naverLocalSearchClient.searchLocal(query, 5);
+        try {
+            NaverSearchResponse response = naverLocalSearchClient.searchLocal(query, 5);
 
-        List<PlaceResponseDto> locationInfoList = response.getItems().stream()
-                .map(item -> new PlaceResponseDto(
-                        item.getTitle().replaceAll("<(/)?b>", ""), // <b> 태그 제거
-                        item.getRoadAddress(),
-                        Double.parseDouble(item.getMapy()),
-                        Double.parseDouble(item.getMapx())))
-                .collect(Collectors.toList());
+            List<PlaceResponseDto> locationInfoList = response.getItems().stream()
+                    .map(item -> new PlaceResponseDto(
+                            item.getTitle().replaceAll("<(/)?b>", ""), // <b> 태그 제거
+                            item.getRoadAddress(),
+                            Double.parseDouble(item.getMapy()),
+                            Double.parseDouble(item.getMapx())))
+                    .collect(Collectors.toList());
 
-        log.info(locationInfoList.toString());
-        return locationInfoList;
+            log.info(locationInfoList.toString());
+            return locationInfoList;
+        }  catch (FeignException e) {
+            log.error("네이버 API 호출 오류: {}", e.getMessage(), e);
+            throw new NaverApiException(ExceptionCode.NAVER_API_ERROR, e);
+        } catch (Exception e) {
+            log.error("알 수 없는 오류: {}", e.getMessage(), e);
+            throw new NaverApiException(ExceptionCode.INTERNAL_SEVER_ERROR, e);
+        }
     }
 }
