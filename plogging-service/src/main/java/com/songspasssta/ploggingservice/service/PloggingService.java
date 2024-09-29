88818c29b6a5f2a9aef1f0ee5b6248f24e74710f
Hagoeun <sera1193@naver.com>
2024-09-29 22:32:48 +0900

    feat: plogging 저장 service

--- plogging-service/src/main/java/com/songspasssta/ploggingservice/service/PloggingService.java
+++ plogging-service/src/main/java/com/songspasssta/ploggingservice/service/PloggingService.java
@@ -1,6 +1,8 @@
 package com.songspasssta.ploggingservice.service;
 
+import com.songspasssta.ploggingservice.domain.Plogging;
 import com.songspasssta.ploggingservice.domain.repository.PloggingRepository;
+import com.songspasssta.ploggingservice.dto.request.PloggingRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +16,19 @@ public class PloggingService {
 
     public void deleteAllByMemberId(final Long memberId) {
         ploggingRepository.deleteByMemberId(memberId);
+    }
+
+    public void savePlogging(final Long memberId, final PloggingRequest ploggingRequest) {
+
+        // TODO image url 저장 로직 필요
+        final Plogging plogging = new Plogging(
+                memberId,
+                ploggingRequest.getStartRoadAddr(),
+                ploggingRequest.getEndRoadAddr(),
+                String.valueOf(1),
+                ploggingRequest.getPloggingTime()
+        );
+
+        ploggingRepository.save(plogging);
     }
 }
