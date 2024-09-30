d568b6965e03e7bbfb6cba60628dab347ea47a46
Hagoeun <sera1193@naver.com>
2024-09-30 22:06:30 +0900

    fix: security 수정

--- member-service/src/main/java/com/songspasssta/memberservice/config/SecurityConfig.java
+++ member-service/src/main/java/com/songspasssta/memberservice/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
                 .logout(AbstractHttpConfigurer::disable)
                 .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/webjars/**", "/member/v1/api-docs", "/api/v1/login/**", "/api/v1/renew").permitAll()
+                        .requestMatchers("/webjars/**", "/member/v1/api-docs", "/api/v1/login/**", "/api/v1/renew", "/api/v1/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(exceptionHandling -> exceptionHandling
