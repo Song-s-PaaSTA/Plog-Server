ddc26d069d0681a4502736292716cbe5dddca71b
Hagoeun <sera1193@naver.com>
2024-09-30 21:28:31 +0900

    fix: request 수정

--- plogging-service/src/main/java/com/songspasssta/ploggingservice/dto/request/PloggingRequest.java
+++ plogging-service/src/main/java/com/songspasssta/ploggingservice/dto/request/PloggingRequest.java
@@ -1,11 +1,13 @@
 package com.songspasssta.ploggingservice.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalTime;
 
@@ -22,6 +24,7 @@ public class PloggingRequest {
     @Size(max = 100, message = "도착지는 100자 이내로 입력해주세요.")
     private String endRoadAddr;
 
-    @NotBlank(message = "플로깅 시간을 입력해주세요.")
+    @NotNull(message = "플로깅 시간을 입력해주세요.")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
     private LocalTime ploggingTime;
 }
