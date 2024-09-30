8e90c741d5457f01db78b43de12f512312c023f1
Hagoeun <sera1193@naver.com>
2024-09-30 19:18:36 +0900

    fix: webflux security 설정 수정

--- apigateway-service/src/main/java/com/songspasssta/apigatewayservice/config/WebFluxSecurityConfig.java
+++ apigateway-service/src/main/java/com/songspasssta/apigatewayservice/config/WebFluxSecurityConfig.java
@@ -1,14 +1,17 @@
 package com.songspasssta.apigatewayservice.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.authorization.ReactiveAuthorizationManager;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
-import org.springframework.security.web.server.authorization.AuthorizationContext;
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
 
+@Configuration
+@RequiredArgsConstructor
 @EnableWebFluxSecurity
 public class WebFluxSecurityConfig {
 
@@ -16,16 +19,17 @@ public class WebFluxSecurityConfig {
             "/", "/csrf",
             "/member-service/login",
             "/?*-service/actuator/?*", "/actuator/?*",
-            "/v3/api-docs/**", "/?*-service/v3/api-docs", "**/configuration/*", "/swagger*/**", "/webjars/**"
+            "/v3/api-docs/**", "/?*-service/v3/api-docs", "/swagger*/**", "/webjars/**"
     };
 
     @Bean
-    public SecurityWebFilterChain configure(ServerHttpSecurity http, ReactiveAuthorizationManager<AuthorizationContext> check) throws Exception {
+    public SecurityWebFilterChain configure(ServerHttpSecurity http) {
         return http
                 .csrf(csrf -> csrf.disable())
                 .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()))
                 .formLogin(formLogin -> formLogin.disable())
                 .httpBasic(httpBasic -> httpBasic.authenticationEntryPoint(new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED)))
+                .securityContextRepository(NoOpServerSecurityContextRepository.getInstance()) //
                 .authorizeExchange(auth -> auth
                         .pathMatchers(PERMIT_ALL_ANTPATTERNS).permitAll()
                 )
